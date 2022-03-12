1ad0de493b4f81f10b2e1809c27d4f2d42809a8d
simonholmes001 <simonholmesabc@gmail.com>
2022-03-12 06:53:40 +0100

    Added styling, fixes AB#354

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/Home';
 import Setup from './pages/Setup';
 import Transactions from './pages/Transactions';
 import Minting from './pages/Minting';
-import Header from './components/Header';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
       <Header />
         <main>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/setup">
-            <Setup />
-          </Route>
-          <Route path="/transactions">
-            <Transactions />
-          </Route>
-          <Route path="/minting">
-            <Minting />
-          </Route>
+          <Switch>
+            <Route path='/' exact>
+              <Redirect to='/home' />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/setup">
+              <Setup />
+            </Route>
+            <Route path="/transactions">
+              <Transactions />
+            </Route>
+            <Route path="/minting">
+              <Minting />
+            </Route>
+          </Switch>
         </main>
     </div>
   );
