d3fe4959387835a3f971cb714f1e1501517fee26
simonholmes001 <simonholmesabc@gmail.com>
2022-03-13 11:39:12 +0100

    Feature: setup GET request to query the script address, fixes AB#363

--- src/pages/QueryScriptAddr.js
+++ src/pages/QueryScriptAddr.js
@@ -1,9 +1,31 @@
-import styles from './Query.module.css'
+import React, { useState } from "react";
+import styles from "./Query.module.css";
+
+import BlockchainAPI from "../components/blockchain/BlockchainAPI";
 
 const QueryScriptAddr = () => {
+  const [apiData, setData] = useState([]);
+
+  function fetchBlockchainData() {
+    fetch("http://167.86.98.239:8000/query/cardano-explorer")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  }
   return (
     <div className={styles.query}>
       <h1>Query Script Address</h1>
+      <React.Fragment>
+        <section>
+          <button onClick={fetchBlockchainData}>Fetch Data</button>
+        </section>
+        <section>
+          <BlockchainAPI apiData={apiData} />
+        </section>
+      </React.Fragment>
     </div>
   );
 };
