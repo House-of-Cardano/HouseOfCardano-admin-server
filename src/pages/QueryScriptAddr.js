46326a24a236f231005378b5f94d63ebe9a13060
simonholmes001 <simonholmesabc@gmail.com>
2022-03-14 17:33:55 +0100

    Feature: can use and extract the query strings from a client side URL to enter as inputs for a postgres query on the server side, fixes AB#368

--- src/pages/QueryScriptAddr.js
+++ src/pages/QueryScriptAddr.js
@@ -10,7 +10,7 @@ const QueryScriptAddr = () => {
 
   function fetchBlockchainData() {
     fetch(
-      "http://167.86.98.239:8000/query/cardano-explorer/?addr=addr_test1wzhfye4zxffxd59gg0fhjzavy7uuhpul04kr5myavevh29svlsrpc&datumHash=\\xfac96da1bf190d85ae7e7a45b07b95826c3eb91b839564959d8411d4e0dc089c"
+      "http://167.86.98.239:8000/query/cardano-explorer?addr=addr_test1wzhfye4zxffxd59gg0fhjzavy7uuhpul04kr5myavevh29svlsrpc&datumHash=\\xfac96da1bf190d85ae7e7a45b07b95826c3eb91b839564959d8411d4e0dc089c"
     )
       .then((response) => {
         return response.json();
