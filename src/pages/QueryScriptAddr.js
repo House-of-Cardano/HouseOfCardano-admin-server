21b106856ba4a2ba552d93ab58d5ad845b30152c
simonholmes001 <simonholmesabc@gmail.com>
2022-03-14 06:18:59 +0100

    Minor updates

--- src/pages/QueryScriptAddr.js
+++ src/pages/QueryScriptAddr.js
@@ -9,7 +9,9 @@ const QueryScriptAddr = () => {
   const [apiData, setData] = useState([]);
 
   function fetchBlockchainData() {
-    fetch("http://167.86.98.239:8000/query/cardano-explorer")
+    fetch(
+      "http://167.86.98.239:8000/query/cardano-explorer/?addr=addr_test1wzhfye4zxffxd59gg0fhjzavy7uuhpul04kr5myavevh29svlsrpc&datumHash=\\xfac96da1bf190d85ae7e7a45b07b95826c3eb91b839564959d8411d4e0dc089c"
+    )
       .then((response) => {
         return response.json();
       })
@@ -20,9 +22,9 @@ const QueryScriptAddr = () => {
   return (
     <div className={styles.query}>
       <h1>Query Script Address</h1>
-        <section>
-            <QueryBlockchainInput />
-        </section>
+      <section>
+        <QueryBlockchainInput />
+      </section>
       <React.Fragment>
         <section>
           <button onClick={fetchBlockchainData}>Fetch Data</button>
